668081b46600d0e339f4ba8b145e87377fa78675
Subaru25 <tunggmph50566@gmail.com>
2024-11-12 23:25:32 +0700

    update hotel

--- hotel.sql
+++ hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 11, 2024 lúc 03:43 PM
+-- Thời gian đã tạo: Th10 12, 2024 lúc 05:20 PM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -47,7 +47,30 @@ CREATE TABLE `bill` (
 
 INSERT INTO `bill` (`id`, `b_name`, `b_email`, `b_address`, `b_city`, `b_state`, `b_n_card`, `b_creditno`, `b_exp_mnth`, `b_exp_yr`, `b_cvv`) VALUES
 (1, 'Tung Joe', 'tunggmph50566@gmail.com', 'Huong Mai', 'Bac Giang', 'NY', 'Visa', '4111111111111111', 12, 2025, 123),
-(2, 'Natsuki Subaru', 'otaku2005bg@gmail.com', '545 Pham Van Dong', 'Tokyo', 'CA', 'MasterCard', '5500000000000004', 6, 2024, 456);
+(2, 'Natsuki Subaru', 'otaku2005bg@gmail.com', '545 Pham Van Dong', 'Tokyo', 'CA', 'MasterCard', '5500000000000004', 6, 2024, 456),
+(3, 'manh', 'tunggmph50566@gmail.com', '545 phạm văn đồng', 'Bac Giang', 'NY', 'Manh Tùng', '1111-2222-3333-4444', 0, 2024, 252),
+(4, '', '', '', '', '', '', '', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `room_name` varchar(255) NOT NULL,
+  `room_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `user_id`, `room_name`, `room_price`) VALUES
+(1, 101, 'Deluxe Room', 150.00),
+(2, 102, 'Suite Room', 250.00);
 
 -- --------------------------------------------------------
 
@@ -117,6 +140,12 @@ ALTER TABLE `bill`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Chỉ mục cho bảng `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`);
+
+--
 -- Chỉ mục cho bảng `room`
 --
 ALTER TABLE `room`
@@ -137,7 +166,7 @@ ALTER TABLE `signup`
 -- AUTO_INCREMENT cho bảng `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `room`
@@ -150,6 +179,16 @@ ALTER TABLE `room`
 --
 ALTER TABLE `signup`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`cart_id`) REFERENCES `room` (`room_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
